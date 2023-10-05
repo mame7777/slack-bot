ad06fe7cea8ef2a8ebd56d9461b1e43fbc37957c
mame <86618354+mame7777@users.noreply.github.com>
2023-10-05 09:24:24 +0900

    [add] start script

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# OMMAND="source /home/mame77/workspace/slack-bot/env/bin/activate"
+# eval $COMMAND
+
+COMMAND="/home/mame77/workspace/slack-bot/env/bin/python3 /home/mame77/workspace/slack-bot/src/main.py"
+eval $COMMAND
+
