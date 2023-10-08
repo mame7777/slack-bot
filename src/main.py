af1723f2d487a5aa3969035241d328407f703f1f
mame7777 <86618354+mame7777@users.noreply.github.com>
2023-10-08 11:11:55 +0900

    [fix] メンション時の挙動を変更

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import re
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import requests
@@ -19,20 +20,41 @@ USER_DATA = dict()
 
 @app.message("")
 def post_message(message, say):
+    # get posted user name and icon
     user_name = USER_DATA[message["user"]]["name"]
     user_icon = USER_DATA[message["user"]]["img"]
+    
+    # get message text
+    message_text = message["text"]
+    
+    # replace mention
+    pattern = r"<@.*?>"
+    re_result_list = re.findall(pattern, message_text)
+    if re_result_list != []:
+        for re_result in re_result_list:
+            mention_user_id = re_result[2:-1]
+            try:
+                mention_user_name = USER_DATA[mention_user_id]["name"]
+            except KeyError:
+                mention_user_name = "unknown user"
+            message_text = message_text.replace(re_result, "`@" + mention_user_name + "` ")
+    message_text = message_text.replace("!channel", "`@channel`")
+    
+    # get posted channel name
     try:
         channel_name = CHANNEL_DATA[message["channel"]]
     except KeyError:
         channel_name = "unkown channel"
+       
+    # post message
     say(
         channel = POST_CHANNEL_ID,
         username = user_name,
         icon_url = user_icon,
-        text=f"`#{channel_name}`\n {message['text']}"
+        text=f"`#{channel_name}`\n{message_text}"
     )
 
-# mention
+# if mention
 @app.event("app_mention")
 def mention(body, say):
     text = body["event"]["text"]
@@ -42,9 +64,12 @@ def mention(body, say):
         say(
             channel = POST_CHANNEL_ID,
             username = "Slack Bot (reload)",
-            text=f"chanelとuserの情報を更新するよ！"
+            text=f"channelとuserの情報を更新するよ！"
         )
+        
+        # reload channel and user data
         init()
+        
         say(
             channel = POST_CHANNEL_ID,
             username = "Slack Bot (reload)",
@@ -64,7 +89,6 @@ def init():
     headres = {"Authorization": "Bearer " + SLACK_BOT_TOKEN}
     response = requests.get(url, headers=headres)
     response_json = response.json()
-    
     global CHANNEL_DATA
     for i in response_json["channels"]:
         CHANNEL_DATA[i["id"]] = i["name"]
@@ -74,7 +98,6 @@ def init():
     headres = {"Authorization": "Bearer " + SLACK_BOT_TOKEN}
     response = requests.get(url, headers=headres)
     response_json = response.json()
-    
     global USER_DATA
     for i in response_json["members"]:
         try:
